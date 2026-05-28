09b529a09887a4f272338689466c970a2798abf9
elvisbrown-29 <elsbee29@gmail.com>
2026-05-28 13:51:10 +0000

    fix: relocate api config inside public and clean vercel config

--- public/api/config.js
+++ public/api/config.js
@@ -1,5 +1,4 @@
 module.exports = (req, res) => {
-    // Set headers to allow safe dynamic fetches
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -8,7 +7,6 @@ module.exports = (req, res) => {
         return res.status(200).end();
     }
 
-    // Return the backend URL configured securely in the Vercel dashboard
     res.status(200).json({
         backendUrl: process.env.BACKEND_URL || ''
     });
